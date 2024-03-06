2c1d0faa474b6f24ebb9245066169475afe5b57e
marcelnoehre <marcelnoehre@web.de>
2024-03-06 19:04:20 +0100

    login ui

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,18 +9,26 @@ import { MaterialModule } from './modules/material-module';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { ToolbarProfileComponent } from './components/toolbar-profile/toolbar-profile.component';
+import { LoginComponent } from './components/login/login.component';
+import { SpinnerIconComponent } from './components/spinner-icon/spinner-icon.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidenavComponent,
     ToolbarComponent,
-    ToolbarProfileComponent
+    ToolbarProfileComponent,
+    LoginComponent,
+    SpinnerIconComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MaterialModule
   ],
   providers: [],
