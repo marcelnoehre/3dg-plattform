3915b316b28dd0521a676c71244ff47b12792d55
marcelnoehre <marcelnoehre@web.de>
2024-03-06 17:25:13 +0100

    include basic material components

---
+++ frontend/src/app/modules/material-module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatListModule } from '@angular/material/list';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+
+@NgModule({
+	declarations: [],
+	imports: [
+		MatSidenavModule,
+		MatToolbarModule,
+		MatMenuModule,
+		MatSnackBarModule,
+		MatButtonModule,
+		MatCardModule,
+		MatIconModule,
+		MatInputModule
+	],
+	exports: [
+		MatSidenavModule,
+		MatToolbarModule,
+		MatMenuModule,
+		MatListModule,
+		MatSnackBarModule,
+		MatButtonModule,
+		MatCardModule,
+		MatIconModule,
+		MatInputModule
+	]
+})
+export class MaterialModule {}
