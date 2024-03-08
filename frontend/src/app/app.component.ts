79c2827b46f04e7f8cf9c5e54e14dc00c503a2e8
marcelnoehre <marcelnoehre@web.de>
2024-03-08 20:36:55 +0100

    fix sidenav behaviour

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { DeviceService } from './services/device.service';
+import { EventService } from './services/event.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+
+  constructor(
+    private _device: DeviceService,
+    private _event: EventService
+  ) { }
+
+  ngOnInit(): void {
+    this._device.init();
+  }
+
+  @HostListener('document:click', ['$event'])
+	documentClick(event: any): void {
+		this._event.documentClick$.next(event.target);
+	}
+  
 }
