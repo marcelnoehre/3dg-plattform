72307bf9b76cee227b13ab86977e01c120d07995
marcelnoehre <marcelnoehre@web.de>
2024-04-04 17:25:03 +0200

    start a bot process

--- backend/src/controllers/tmp-voice-bot.controller.js
+++ backend/src/controllers/tmp-voice-bot.controller.js
@@ -1,3 +1,30 @@
+const databaseService = require('../database/database.service');
+const { spawn } = require('child_process');
+
+let botProcess;
+let consoleOutput = [];
+
+function startBot() {
+    botProcess = spawn('node', ['/Users/marcelnoehre/Documents/dev/bots/3dg-tmp-voice/index.js']);
+
+    botProcess.stdout.on('data', (data) => {
+        const output = data.toString();
+        console.log(output);
+        consoleOutput.push(output);
+    });
+
+    botProcess.stderr.on('data', (data) => {
+        const output = data.toString();
+        console.error(output);
+        consoleOutput.push(output);
+    });
+
+    botProcess.on('close', (code) => {
+        console.log(`Bot process exited with code ${code}`);
+        botProcess = null;
+    });
+}
+
 module.exports = {
 
 }
