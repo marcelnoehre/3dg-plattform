f36d0825a3a5b3a441279bf0a2145ddb287cb955
marcelnoehre <marcelnoehre@web.de>
2024-04-04 17:24:03 +0200

    setup tmp-voice-bot backend

---
+++ backend/src/routes/tmp-voice-bot.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const tmpVoiceBotController = require('../controllers/tmp-voice-bot.controller');
+const jwtAuth = require('../auth/jwtAuth');
+
+module.exports = router;
