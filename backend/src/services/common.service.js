6359ccee3bae55462a80d8059cb26943591fc064
marcelnoehre <marcelnoehre@web.de>
2024-04-05 10:42:02 +0200

    service to read and write json files

---
+++ backend/src/services/common.service.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const util = require('util');
+const readFileAsync = util.promisify(fs.readFile);
+
+async function readJSONFile(path) {
+    try {
+        const data = await readFileAsync(path, 'utf8');
+        return JSON.parse(data);
+    } catch (err) {
+        throw new Error('Error accessing file: ' + err);
+    }
+}
+
+async function updateJSONFile(path, json) {
+    fs.writeFile(path, JSON.stringify(database, null, 2), 'utf8', (err) => {
+        if (err) {
+            throw new Error('Error saving file: ' + err);
+        }
+    });
+}
+
+module.exports = {
+    readJSONFile,
+    updateJSONFile
+}
