db4e7b71faa3fc29796ce1c3551bfaf5804bdbf7
marcelnoehre <marcelnoehre@web.de>
2024-04-25 14:02:08 +0200

    block apps via permission

--- frontend/src/app/interfaces/app.interface.ts
+++ frontend/src/app/interfaces/app.interface.ts
@@ -2,10 +2,12 @@ import { AppColor } from "../enums/app-color.enum";
 import { AppIcon } from "../enums/app-icon.enum";
 import { AppItem } from "../enums/app-item.enum";
 import { AppRoute } from "../enums/app-route.enum";
+import { Permission } from "../enums/permission.enum";
 
 export interface App {
 	item: AppItem;
 	icon: AppIcon;
 	route: AppRoute;
 	color: AppColor;
+	permission: Permission;
 }
