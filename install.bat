c98192341db07be2016fa4379377a13bbc8a7c89
marcelnoehre <marcelnoehre@web.de>
2024-04-25 14:06:42 +0200

    install + start script

---
+++ install.bat
@@ -0,0 +1,3 @@
+@echo off
+start cmd /k "cd frontend && npm install && exit"
+start cmd /k "cd backend && npm install && exit"
---
+++ start.bat
@@ -0,0 +1,3 @@
+@echo off
+start cmd /k "cd frontend && npm run start"
+start cmd /k "cd backend && npm run start"
